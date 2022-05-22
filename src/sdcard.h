0ff774a2ce3927b4957b70fa83872546703741b4
Eric Callahan <arksine.code@gmail.com>
2022-05-22 15:31:19 -0400

    sdcard: initial implementation
    Add support for firmware uploads from an SD Card via SPI.
    
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

---
+++ src/sdcard.h
@@ -0,0 +1,24 @@
+#ifndef SDCARD_H
+#define SDCARD_H
+
+#include <stdint.h>
+
+#define SDCMD_GO_IDLE_STATE     0
+#define SDCMD_SEND_IF_COND      8
+#define SDCMD_SEND_CSD          9
+#define SDCMD_SEND_OP_COND      41
+#define SDCMD_SEND_STATUS       13
+#define SDCMD_SET_BLOCKLEN      16
+#define SDCMD_READ_SINGLE_BLOCK 17
+#define SDCMD_WRITE_BLOCK       24
+#define SDCMD_APP_CMD           55
+#define SDCMD_READ_OCR          58
+#define SDCMD_CRC_ON_OFF        59
+#define SD_SECTOR_SIZE          512
+
+uint8_t sdcard_write_sector(uint8_t* buf, uint32_t sector);
+uint8_t sdcard_read_sector(uint8_t *buf, uint32_t sector);
+uint8_t sdcard_init(void);
+void sdcard_deinit(void);
+
+#endif
